f01d764741cfee91e123d56bcfc826d93235a5e0
stevebresnick <stevebresnick@gmail.com>
2019-05-15 23:39:15 -0400

    autoloader fix

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,8 +4,8 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-<<<<<<< HEAD
-return ComposerAutoloaderInitb8f37a985c453bb7543f66b96cdc9ad2::getLoader();
-=======
-return ComposerAutoloaderInitDrupal8::getLoader();
->>>>>>> pantheon-drops-8/master
+//<<<<<<< HEAD
+//return ComposerAutoloaderInitb8f37a985c453bb7543f66b96cdc9ad2::getLoader();
+//=======
+//return ComposerAutoloaderInitDrupal8::getLoader();
+//>>>>>>> pantheon-drops-8/master
